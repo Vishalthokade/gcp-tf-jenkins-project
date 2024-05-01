a955eb77c730c01a7467a79a2e4da22503a449ea
Vishal Thokade <vishalthokade1996@gmail.com>
2024-05-01 19:19:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my_gcs_bucket1"{
 name ="gcp-tf-jenkins-bucket"
-project = "My First Project"
+project = "	central-rampart-421909-r3"
 location = "US"
 force_destroy= true
 public_access_prevention = "enforced"
