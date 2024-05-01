c6f4d39723426d3a445a9a9f9586999dde2c5003
Vishal Thokade <vishalthokade1996@gmail.com>
2024-05-01 20:14:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ public_access_prevention = "enforced"
 }
 
 resource "google_storage_bucket" "my_gcs_bucket2"{
-name                     ="gcp-tf-jenkins-bucket2"
+name                     ="gcp-tf-jenkins-bucket2-renaming-bucket"
 project                  = "central-rampart-421909-r3"
 location                 = "US"
 force_destroy            = true
