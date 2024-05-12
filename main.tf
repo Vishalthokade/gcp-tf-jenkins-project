e0c0faf92b6d22aba40a1f40cdd68cbd012d80e8
Vishal Thokade <vishalthokade1996@gmail.com>
2024-05-12 12:18:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,4 @@ force_destroy            = true
 public_access_prevention = "enforced"
 }
 
-resource "google_storage_bucket" "my_gcs_bucket2"{
-name                     ="gcp-tf-jenkins-bucket2-renaming-bucket"
-project                  = "central-rampart-421909-r3"
-location                 = "US"
-force_destroy            = true
-public_access_prevention = "enforced"
-}
 ##comment added to check if end to end cicd is implemented or not
